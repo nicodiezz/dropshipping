9ff46d3cf78aae2ffdf9b2c0ec9fb72a6d67aab6
colosoler <162416581+colosoler@users.noreply.github.com>
2024-09-19 16:27:13 -0300

    Update get-articles.php

--- libs/get-articles.php
+++ libs/get-articles.php
@@ -1,14 +1,24 @@
 <?php
+
 session_start(['read_and_close'=>true]);
 define('DR',$_SERVER['DOCUMENT_ROOT']);
 if(!isset($_SESSION['ID']))
 	require DR.'/libs/header-location.php';
 require '../libs/db.php';
-$rawRes=$db->query("SELECT * FROM `pd_articulos` WHERE `disponible`<>0 AND `vendedorID`=".$_SESSION['ID']);
-if($rawRes->num_rows){
-	$rejunte=[];
-	while($art=$rawRes->fetch_assoc())
-		$rejunte[]=$art;
-	die(json_encode($rejunte));
+$categorias_response=$db->query("SELECT * FROM pd_vendedores_categorias WHERE vendedorID=".$_SESSION['ID']);
+
+$articulos=[];
+if($categorias_response && $categorias_response->num_rows){
+	while($categoria = $categorias_response->fetch_assoc()) {
+		$articulo_response= $db->query("SELECT * FROM pd_articulos WHERE categoriaID=".$categoria["categoriaID"]);
+		if($articulo_response && $articulo_response->num_rows){
+			while ($articulo = $articulo_response->fetch_assoc()){
+				$articulos[] = $articulo_response->fetch_assoc();
+			}		
+		}
+	}
+}
+
+if(count($articulos)){
+	die(json_encode($articulos));
 }else die('[]');
-?>
