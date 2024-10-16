f094bb7dbbb349efafe0fe0b75f84a4ede66df02
colosoler <tomasgregorio2005@gmail.com>
2024-10-16 15:40:15 -0300

    add: delete-grup

---
+++ libs/grupos/delete-grup.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start(['read_and_close' => true]);
+$data = json_decode(file_get_contents('php://input'), true);
+
+
+if (!(isset($_SESSION['isAdmin']) && isset($data['ID']))) {
+    require $_SERVER['DOCUMENT_ROOT'].'/libs/header-location.php';
+    exit;
+}
+
+require '../db.php';
+
+if ((int)$data['ID']) {
+    // Solo eliminar el grupo, sin eliminar los artículos relacionados
+    $db->query("DELETE FROM `pd_grupos` WHERE `ID` = ".(int)$data['ID']);
+    $grupos_afectados = $db->affected_rows();
+    
+    if ($grupos_afectados > 0) {
+        echo "Grupo eliminado correctamente.";
+    } else {
+        echo 0; 
+    }
+} else {
+    echo 0; // No se proporcionó un ID válido
+}
+
+?>
